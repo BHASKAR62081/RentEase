dca358a84dae06a4c7dbf37887be921064235e4b
Bhaskar012 <bhaskar62081mallick@gmail.com>
2025-05-04 03:05:48 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = mapToken;
+
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 10 }).setHTML("<h5>You'll live here!!</h5>")
+  )
+  .addTo(map);
